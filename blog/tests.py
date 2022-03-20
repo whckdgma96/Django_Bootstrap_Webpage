1275696c67c19b1ee8fca439db0b066a2186b991
CHC <whckdgma96@gmail.com>
2022-03-20 12:06:43 +0900

    내비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -6,19 +6,37 @@ class TestView(TestCase):
     def SetUp(self):
         self.Client = Client()
 
+    def navbar_test(self,soup):
+        navbar = soup.nav
+        self.assertIn("Blog", navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='ChangHeum.Cho')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/blog/')
+
         # 1.2 정상적으로 페이지가 로드된다.
         self.assertEqual(response.status_code, 200)
+
         # 1.3 페이지 타이틀은 Blog 이다.
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
+
         # 1.4 내비게이션 바가 있다.
-        navbar = soup.nav
         # 1.5 Blog, About Me 라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -63,9 +81,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn("About Me", navbar.text)
+        self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어있다.
         self.assertIn(post_001.title, soup.title.text)
